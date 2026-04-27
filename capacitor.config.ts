f7d8977203f9e1a1eda7828c7fec016f576f28cf
senemdenem <koknerenes@hotmail.com>
2026-04-27 10:43:32 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.primetv.iptv',
+  appName: 'PrimeTV',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
